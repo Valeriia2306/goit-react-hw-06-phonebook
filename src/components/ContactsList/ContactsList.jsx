ff7eae67e8d0ffb64bc6e979cf104e0450d0a336
Valeriia Frolova <valeriiats1989@gmail.com>
2023-04-03 16:51:24 +0300

    fix

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,10 +1,9 @@
 import { useSelector } from 'react-redux';
 import Contact from '../ContactItem/ContactItem';
 import filterOfContactsArray from '../../service/filterOfContactsArray';
-
-import { ContactsList, LabelItem } from './ContactsList.styled';
 import { getContacts, getFilter } from '../../Redux/selectors';
-
+// styles
+import { ContactsList, LabelItem } from './ContactsList.styled';
 const ContactList = () => {
   const contacts = useSelector(getContacts);
   const toFilter = useSelector(getFilter);
